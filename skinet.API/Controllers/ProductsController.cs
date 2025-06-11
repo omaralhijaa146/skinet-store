762bee7b65a09009eca7d9b92945476ac4c3b3da
omar abu alhijaa <omaribraheem222@hotmail.com>
2025-06-11 19:35:15 +0300

    Initial Commit

---
+++ skinet.API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using skinet.Core.Entities;
+using skinet.Infrastructure.Data;
+
+
+namespace skinet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var result =await  _context.Products.ToListAsync();
+            return result;
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FirstOrDefaultAsync(x=>x.Id == id);
+        }
+        
+    }
+}
---
+++ skinet.Core/Entities/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace skinet.Core.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
